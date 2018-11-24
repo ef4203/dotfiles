02fd5bc46890a90e3d32c5f88855d8ba46ed81ba
Elias F <elias_frank@hotmail.com>
2018-11-24 20:40:31 +0100

    added cool larps stuff

--- .scripts/rofi-macro.sh
+++ .scripts/rofi-macro.sh
@@ -3,5 +3,4 @@
 rofi -show run -fake-transparency -width 100 -padding 800 \
     -color-window "#77000000,#2f343f,#77000000" \
     -color-normal "#00000000,#f3f4f5,#00000000,#f3f4f5,#3f343f" \
-    -font "System San Francisco Display 24"\
     -lines 5
