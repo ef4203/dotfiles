d497e7ff6cb1c0736fd79f82b16a5d559f717c27
Elias F <32647755+ef4203@users.noreply.github.com>
2019-01-25 23:19:28 +0100

    Added addtionawl dotfiles for sync

--- install.sh
+++ install.sh
@@ -29,5 +29,16 @@ done
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     echo "Installing Vundle.vim..."
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    echo "IMPORTANT: don't forget to run :PluginInstall first time you open vim"
 fi
+
+# Additional dotfiles
+printf "Installing i3 config...\n"
+ln -s `pwd`/.config/i3/config ~/.config/i3/config
+printf "Installing ranger config...\n"
+ln -s `pwd`/.config/ranger/rc.conf ~/.config/ranger/rc.conf
+printf "Installing mpv config...\n"
+ln -s `pwd`/.config/mpv/input.conf ~/.config/mpv/input.conf
+printf "Installing zathura config...\n"
+ln -s `pwd`/.config/zathura/zathurarc ~/.config/zathura/zathurarc
 
