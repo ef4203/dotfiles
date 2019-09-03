208f0636bda1171ef7c4a55adde3e28974ac55ae
Elias F <32647755+ef4203@users.noreply.github.com>
2019-09-03 22:21:47 +0200

    Updated installscript to ask the user

--- Windows/install.ps1
+++ Windows/install.ps1
@@ -1,5 +1,8 @@
 #Requires -RunAsAdministrator
 
+# Add VB assembly for dialgue boxes
+Add-Type -AssemblyName Microsoft.VisualBasic
+
 # Install and configure choco package manager
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
@@ -11,27 +14,46 @@ choco install firefox
 choco install sumatrapdf.install
 
 # Dev Stuff
-choco install chromium
-choco install vscode
-choco install azure-data-studio
-choco install git.install --params "/NoShellIntegration /GitOnlyOnPath /WindowsTerminal"
+$devenabled = [Microsoft.VisualBasic.Interaction]::MsgBox("Will this be a DEVELOPER machine? (This will install Chromium, VSCode, ADS, Git, Visual Studio, SQL)",'YesNo,Question', "Software Install Script")
+If ($devenabled -eq "Yes")
+{
+    choco install chromium
+    choco install vscode
+    choco install azure-data-studio
+    choco install git.install --params "/NoShellIntegration /GitOnlyOnPath /WindowsTerminal"
 
-# Personal Stuff
-choco install keepass
-choco install sharex
-choco install spotify
-choco install ffmpeg
-choco install youtube-dl
-choco install obs-studio
+    # Download MSSQL
+    Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/?linkid=853016 -OutFile SQL-Server-DEV.exe
 
-# Download MSSQL
-Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/?linkid=853016 -OutFile SQL-Server-DEV.exe
+    # Download Teams
+    Invoke-WebRequest -Uri https://statics.teams.microsoft.com/production-windows-x64/1.2.00.21068/Teams_windows_x64.exe -OutFile Teams_windows_x64.exe
 
-# Download Teams
-Invoke-WebRequest -Uri https://statics.teams.microsoft.com/production-windows-x64/1.2.00.21068/Teams_windows_x64.exe -OutFile Teams_windows_x64.exe
+    # Download Visual Studio
+    Invoke-WebRequest -Uri https://download.visualstudio.microsoft.com/download/pr/b6a85ca1-3add-4391-9d24-27fea671c700/ea7ec3e24daf9852dabe671a8c8b7131/vs_community.exe -OutFile vs_community.exe
+}
 
-# Download Visual Studio
-Invoke-WebRequest -Uri https://download.visualstudio.microsoft.com/download/pr/b6a85ca1-3add-4391-9d24-27fea671c700/ea7ec3e24daf9852dabe671a8c8b7131/vs_community.exe -OutFile vs_community.exe
+$additionals = [Microsoft.VisualBasic.Interaction]::MsgBox("Do you want to install additional software? (This will install Keepass, sharex, spotify, ffmpeg, youtube-dl, OBS)",'YesNo,Question', "Software Install Script")
+If ($additionals -eq "Yes")
+{
+    # Personal Stuff
+    choco install keepass
+    choco install sharex
+    choco install spotify
+    choco install ffmpeg
+    choco install youtube-dl
+    choco install obs-studio
+
+    # Remove bloated shell keys
+    New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
+
+    Push-Location
+    Set-Location HKCR:\Directory\shell
+    Remove-Item -Recurse ShareX
+
+    Set-Location -LiteralPath HKCR:\*\shell
+    Remove-Item -Recurse ShareX
+    Pop-Location
+}
 
 # Enable "Show File Extensions" for Explorer
 Push-Location
@@ -40,13 +62,3 @@ Set-ItemProperty . HideFileExt "0"
 Set-ItemProperty . ShowStatusBar "0"
 Pop-Location
 
-# Remove bloated shell keys
-New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
-
-Push-Location
-Set-Location HKCR:\Directory\shell
-Remove-Item -Recurse ShareX
-
-Set-Location -LiteralPath HKCR:\*\shell
-Remove-Item -Recurse ShareX
-Pop-Location
