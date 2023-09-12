1cb7a49b2e98e123ef69e9dbd71d6420eeb7c9a3
Elias Frank <32647755+ef4203@users.noreply.github.com>
2023-09-12 18:39:08 +0200

    Updated linux files

--- Linux/install.sh
+++ Linux/install.sh
@@ -1,40 +1,25 @@
 #!/bin/bash
 make_home_symlink() {
     printf "Installing %s...\n" $1
-    unlink ~/$2 2>/dev/null
-    ln -s `pwd`/$1 ~/$2 2>/dev/null
+    ln -sf "$PWD/$2" "$HOME/$2" 2>/dev/null
 }
 
 dotfiles=(
-    .bash_logout
-    .bashrc
-    .vimrc
-    .profile
+   .Xdefaults
+   .vimrc
+   .config/fish
 )
 
 for dotfile in ${dotfiles[*]}; do
     make_home_symlink $dotfile $dotfile
 done
 
-# Might be redundant on some systems that use tty login
-make_home_symlink .profile .bash_profile
-
-if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-    echo "Installing Vundle.vim..."
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    echo "IMPORTANT: don't forget to run :PluginInstall first time you open vim"
-fi
-
 echo "Installing gitconfig..."
 if [ ! -f ~/.gitconfig ]; then
     cp .gitconfig ~/.gitconfig
     echo "Don't forget to set your git stuff, eg. user.name and user.email"
 else
-    if [ ! $(grep alias ~/.gitconfig) ]; then
-        cat .gitconfig >> ~/.gitconfig
-    else
-        echo "IMPORTANT: There's already aliases in your gitconfig, manual intervention required."
-    fi
+    echo "IMPORTANT: There's already aliases in your gitconfig, manual intervention required."
 fi
 
 echo "Done!"
